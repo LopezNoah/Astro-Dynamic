466ef3c9a9f8e7760800ab05f7df05460b1f2f35
nlopez4 <noahtheark2@gmail.com>
2023-05-20 21:13:40 -0700

    Add SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  adapter: node({
+    mode: "standalone"
+  })
+});
